fd7407d948e81df5ce2eed8e8bc73341192916c3
Tiago Vieira <tiago@tvieira.net>
2014-03-14 17:09:49 -0300

    Styles e scripts no estilo Blade

--- app/views/layouts/base.blade.php
+++ app/views/layouts/base.blade.php
@@ -8,29 +8,29 @@
 
         <title>Ala-Laravel</title>
 
-        <link href="{{ URL::to('/'); }}/assets/css/bootstrap.css" rel="stylesheet" />
-        <link href="{{ URL::to('/'); }}/assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
-        <link href="{{ URL::to('/'); }}/assets/css/plugins/morris/morris-0.4.3.min.css" rel="stylesheet" />
-        <link href="{{ URL::to('/'); }}/assets/css/sb-admin.css" rel="stylesheet" />
-        <link href="{{ URL::to('/'); }}/assets/css/chosen.css" rel="stylesheet" />
-        <link href="{{ URL::to('/'); }}/assets/css/prism.css" rel="stylesheet" />
-        <link href="{{ URL::to('/'); }}/assets/css/datepicker3.css" rel="stylesheet" />
-        <link href="{{ URL::to('/'); }}/assets/css/plugins/dataTables/dataTables.bootstrap.css" rel="stylesheet" />
+        {{HTML::style('/assets/css/bootstrap.css') }}
+        {{HTML::style('/assets/font-awesome/css/font-awesome.min.css') }}
+        {{HTML::style('/assets/css/plugins/morris/morris-0.4.3.min.css') }}
+        {{HTML::style('/assets/css/sb-admin.css') }}
+        {{HTML::style('/assets/css/chosen.css') }}
+        {{HTML::style('/assets/css/prism.css') }}
+        {{HTML::style('/assets/css/datepicker3.css') }}
+        {{HTML::style('/assets/css/plugins/dataTables/dataTables.bootstrap.css') }}
 
-        <script src="{{ URL::to('/'); }}/assets/js/jquery-1.10.2.js"></script>
-        <script src="{{ URL::to('/'); }}/assets/js/bootstrap.min.js"></script>
-        <script src="{{ URL::to('/'); }}/assets/js/plugins/metisMenu/jquery.metisMenu.js"></script>
-        <script src="{{ URL::to('/'); }}/assets/js/plugins/morris/raphael-2.1.0.min.js"></script>
-        <script src="{{ URL::to('/'); }}/assets/js/plugins/morris/morris.js"></script>
-        <script src="{{ URL::to('/'); }}/assets/js/sb-admin.js"></script>
-        <script src="{{ URL::to('/'); }}/assets/js/chosen.jquery.js"></script>
-        <script src="{{ URL::to('/'); }}/assets/js/prism.js"></script>
-        <script src="{{ URL::to('/'); }}/assets/js/bootstrap-datepicker.js"></script>
-        <script src="{{ URL::to('/'); }}/assets/js/datepicker-locales/bootstrap-datepicker.pt-BR.js"></script>
-        <script src="{{ URL::to('/'); }}/assets/js/bootstrap-datepicker.js"></script>
-        <script src="{{ URL::to('/'); }}/assets/js/plugins/dataTables/dataTables.bootstrap.js"></script>
-        <script src="{{ URL::to('/'); }}/assets/js/plugins/dataTables/jquery.dataTables.js"></script>
-        <script src="{{ URL::to('/'); }}/assets/js/plugins/dataTables/bootstrap-dataTables-paging.js"></script>
+        {{HTML::script('/assets/js/jquery-1.10.2.js') }}
+        {{HTML::script('/assets/js/bootstrap.min.js') }}
+        {{HTML::script('/assets/js/plugins/metisMenu/jquery.metisMenu.js') }}
+        {{HTML::script('/assets/js/plugins/morris/raphael-2.1.0.min.js') }}
+        {{HTML::script('/assets/js/plugins/morris/morris.js') }}
+        {{HTML::script('/assets/js/sb-admin.js') }}
+        {{HTML::script('/assets/js/chosen.jquery.js') }}
+        {{HTML::script('/assets/js/prism.js') }}
+        {{HTML::script('/assets/js/bootstrap-datepicker.js') }}
+        {{HTML::script('/assets/js/datepicker-locales/bootstrap-datepicker.pt-BR.js') }}
+        {{HTML::script('/assets/js/bootstrap-datepicker.js') }}
+        {{HTML::script('assets/js/plugins/dataTables/dataTables.bootstrap.js') }}
+        {{HTML::script('/assets/js/plugins/dataTables/jquery.dataTables.js') }}
+        {{HTML::script('/assets/js/plugins/dataTables/bootstrap-dataTables-paging.js') }}
 
     </head>
 
